2c386176b9aab10c5d672685149200ef031324ae
jstamn <jstamn@wisc.edu>
2018-04-09 15:14:33 -0500

    Added docstrings to graph

--- src/Graph.java
+++ src/Graph.java
@@ -19,57 +19,104 @@ public class Graph<E> implements GraphADT<E> {
      * Instance variables and constructors
      */
 
-    /**
-     * {@inheritDoc}
-     */
+	/**
+	 * Add new vertex to the graph
+	 * 
+	 * Valid argument conditions:
+	 * 1. vertex should be non-null
+	 * 2. vertex should not already exist in the graph 
+	 * 
+	 * @param vertex the vertex to be added
+	 * @return vertex if vertex added, else return null if vertex can not be added (also if valid conditions are violated)
+	 */
     @Override
     public E addVertex(E vertex) {
         
     }
 
     /**
-     * {@inheritDoc}
-     */
+	 * Remove the vertex and associated edge associations from the graph
+	 * 
+	 * Valid argument conditions:
+	 * 1. vertex should be non-null
+	 * 2. vertex should exist in the graph 
+	 *  
+	 * @param vertex the vertex to be removed
+	 * @return vertex if vertex removed, else return null if vertex and associated edges can not be removed (also if valid conditions are violated)
+	 */
     @Override
     public E removeVertex(E vertex) {
         
     }
 
     /**
-     * {@inheritDoc}
-     */
+	 * Add an edge between two vertices (edge is undirected and unweighted)
+	 * 
+	 * Valid argument conditions:
+	 * 1. both the vertices should exist in the graph
+	 * 2. vertex1 should not equal vertex2
+	 *  
+	 * @param vertex1 the first vertex
+	 * @param vertex2 the second vertex
+	 * @return true if edge added, else return false if edge can not be added (also if valid conditions are violated)
+	 */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
         
     }    
 
     /**
-     * {@inheritDoc}
-     */
+	 * Remove the edge between two vertices (edge is undirected and unweighted)
+	 * 
+	 * Valid argument conditions:
+	 * 1. both the vertices should exist in the graph
+	 * 2. vertex1 should not equal vertex2
+	 *  
+	 * @param vertex1 the first vertex
+	 * @param vertex2 the second vertex
+	 * @return true if edge removed, else return false if edge can not be removed (also if valid conditions are violated)
+	 */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
         
     }
 
     /**
-     * {@inheritDoc}
-     */
+	 * Check whether the two vertices are adjacent
+	 * 
+	 * Valid argument conditions:
+	 * 1. both the vertices should exist in the graph
+	 * 2. vertex1 should not equal vertex2
+	 *  
+	 * @param vertex1 the first vertex
+	 * @param vertex2 the second vertex
+	 * @return true if both the vertices have an edge with each other, else return false if vertex1 and vertex2 are not connected (also if valid conditions are violated)
+	 */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
         
     }
 
     /**
-     * {@inheritDoc}
-     */
+	 * Get all the neighbor vertices of a vertex
+	 * 
+	 * Valid argument conditions:
+	 * 1. vertex is not null
+	 * 2. vertex exists
+	 * 
+	 * @param vertex the vertex
+	 * @return an iterable for all the immediate connected neighbor vertices
+	 */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
         
     }
 
     /**
-     * {@inheritDoc}
-     */
+	 * Get all the vertices in the graph
+	 * 
+	 * @return an iterable for all the vertices
+	 */
     @Override
     public Iterable<E> getAllVertices() {
         
