aff3f7b1291d75294d4adbf0a21ade44b3973e27
tschmidt6 <36741011+tschmidt6@users.noreply.github.com>
2018-04-09 17:56:45 -0500

    Update Graph.java
    Added functions, currently passes the default tests. More testing will be added.

--- src/Graph.java
+++ src/Graph.java
@@ -2,8 +2,25 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
-//Test git
-
+////////////////////////////////////////////////////////////////////////////
+// Semester:         CS400 Spring 2018
+// PROJECT:          P3 Dictionary Graph
+// FILES:            Graph.java
+//                   GraphProcessor.java
+//                   GraphTest.java
+//                   WordProcessor.java
+//
+// USER:             tschmidt6@wisc.edu | Teryl Schmidt
+//		     YOUR EMAIL HERE
+//		     ...
+//		     ...
+//
+//
+// Instructor:       Deb Deppeler (deppeler@cs.wisc.edu)
+// Bugs:             no known bugs
+//
+// Due:				2018 Apr 16, 2018 Graph.java 
+//////////////////////////// 80 columns wide //////////////////////////////////
 
 /**
  * Undirected and unweighted graph implementation
@@ -18,6 +35,11 @@ public class Graph<E> implements GraphADT<E> {
     /**
      * Instance variables and constructors
      */
+	final private HashMap<E, Set<E>> adjacencyList;
+	
+ 	public Graph() {
+		this.adjacencyList = new HashMap<>();
+	}
 
 	/**
 	 * Add new vertex to the graph
@@ -31,12 +53,17 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public E addVertex(E vertex) {
-    	//TODO: Check if vertex exists in graph
+    	// Can't add null vertex
         if (vertex == null) {
         	return null;
         }
-        
-        
+	// No duplicate vertices
+	if (this.adjacencyList.containsKey(vertex)) {
+		return null;
+	}
+	// Add vertex
+	this.adjacencyList.put(vertex, new HashSet<E>());
+	return vertex;
     }
 
     /**
@@ -51,9 +78,20 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public E removeVertex(E vertex) {
+	// Can't remove null
     	if (vertex == null) {
         	return null;
         }
+	// Must exist in graph to remove
+	if (!this.adjacencyList.containsKey(vertex)) {
+		return null;
+	}
+	// Remove vertex
+	this.adjacencyList.remove(vertex);
+	for (E currentVertex: this.getAllVertices()) {
+		this.adjacencyList.get(currentVertex).remove(vertex);
+	}
+	return vertex;
         
     }
 
@@ -70,10 +108,18 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
-    	if (vertex1 == vertex2) {
-    		return false;
-    	}
-        
+	// Check validity of vertices before testing
+	if (vertex1 == null || vertex2 == null || vertex1 == vertex2) {
+		return false;
+	}
+	// Vertices must be in the graph to be able to tell if edge between them
+	if (!this.adjacencyList.containsKey(vertex1) || !this.adjacencyList.containsKey(vertex2)) {
+		return false;
+	}
+	//Undirected graph implementation so adding edge from V1 -> V2 also adds edge from V1 <- V2
+	this.adjacencyList.get(vertex1).add(vertex2);
+	this.adjacencyList.get(vertex2).add(vertex1);
+	return true;  
     }    
 
     /**
@@ -89,9 +135,18 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
-    	if (vertex1 == vertex2) {
-    		return false;
-    	}
+	// Check validity of vertices before testing
+    	if (vertex1 == null || vertex2 == null || vertex1 == vertex2) {
+		return false;
+	}
+	// Vertices must be in the graph to be able to remove edge between them
+	if (!this.adjacencyList.containsKey(vertex1) || !this.adjacencyList.containsKey(vertex2)) {
+		return false;
+	}
+	//Undirected graph implementation so removing edge from V1 -> V2 also removes edge from V1 <- V2
+	this.adjacencyList.get(vertex1).remove(vertex2);
+	this.adjacencyList.get(vertex2).remove(vertex1);
+	return true;
         
     }
 
@@ -108,10 +163,15 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-    	if (vertex1 == vertex2) {
-    		return false;
-    	}
-        
+	// Check validity of vertices before testing
+    	if (vertex1 == null || vertex2 == null || vertex1 == vertex2) {
+		return false;
+	}  
+	// Vertices must be in the graph to be able to tell if they are adjacent
+	if (!this.adjacencyList.containsKey(vertex1) || !this.adjacencyList.containsKey(vertex2)) {
+		return false;
+	}
+	return this.adjacencyList.get(vertex1).contains(vertex2);
     }
 
     /**
@@ -126,7 +186,7 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
-        
+        return this.adjacencyList.get(vertex);
     }
 
     /**
@@ -136,7 +196,7 @@ public class Graph<E> implements GraphADT<E> {
 	 */
     @Override
     public Iterable<E> getAllVertices() {
-        
+        return this.adjacencyList.keySet();
     }
 
 }
