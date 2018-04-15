69b95b0edf3e6db2e18955972c9860c49ebd56ec
jesoukup <33041461+jesoukup@users.noreply.github.com>
2018-04-14 19:37:23 -0500

    Add files via upload

---
+++ GraphProcessorTest.java
@@ -0,0 +1,39 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GraphProcessorTest {
+    
+    GraphProcessor gproc = null;
+    
+    /**
+     * @throws java.lang.Exception
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        gproc = new GraphProcessor();
+    }
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+}
